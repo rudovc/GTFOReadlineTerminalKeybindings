bbc747bd06afa9caa319a27361c9346c02fac0d7
Roko Udovč <roko.udovc@outlook.com>
2026-07-02 16:08:21 +0200

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+
+namespace readline_terminal_keybindings;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log = base.Log;
+        Log.LogInfo($"Readline Terminal Keybdingins is loaded");
+    }
+}
